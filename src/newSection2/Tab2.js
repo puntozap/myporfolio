22d81f1df74aaa92d278052a29ad842b30019511
Jose Manuel Rojas Villar <villar88@gmail.com>
2019-07-15 06:29:42 -0400

    loading data last curriculum

--- src/newSection2/Tab2.js
+++ src/newSection2/Tab2.js
@@ -9,7 +9,7 @@ class Tab2 extends React.Component {
                 <Col sm="12" className="pt-5" >
                     <p className="h4 text-justify">Profile</p>
                     <p className="text-justify text-monospace">
-                        I have been programming for 10 years in professional academic mode and 3 years experience in small and big companies around the world, with a company named VILLARTECHNOLOGIES.</p>
+                        I have been programming for 10 years in professional academic mode and 4 years experience in small and big companies around the world, with a company named VILLARTECHNOLOGIES.</p>
                     <p className="text-justify text-monospace">During that time, I developed in Backend, however, I 💓 frontend, because, there is nothing like a tech perfect product from the visual point of view, Therefore, I consider an integral person, because I like as the backend as the frontend.  There is nothing like the  creative or simples  desing ideas in any web project</p>
                     <hr/>
                     <p className="h4 text-justify">Academic information</p>
--- src/newSection2/Tab4.js
+++ src/newSection2/Tab4.js
@@ -13,11 +13,11 @@ class Tab4 extends React.Component {
                 <p className="h4">Porfolio</p>
                 <p>This is my work that made around 4 years in Villartechnologies</p>
                 <Row>
-                    <Col title="It is a CMS Wordpress and my Job was install, edit the CSS theme. It is a Website from Spain" lg={6}><img src={diexsur} width={300} /><p><a className="text-white" href="http://diexsur.com/" target="_blank">Diexsur</a></p></Col>
-                    <Col title="That Work was made with Codeigniter in MVC Arquitecture and modified the CSS interface. It is a Website From Paraguay" lg={6}><img src={joven} width={300} /><p><a className="text-white" href="https://joven.lat/" target="_blank">Joven</a></p></Col>
-                    <Col title="That Work was made in Codeigniter, install an admin template and create a CRM to clean service in Spain" lg={6}><img src={elrondaguel} width={300} /><p><a className="text-white" href="http://control.elrondaguel.es" target="_blank">ElRondaguel</a></p></Col>
-                    <Col title="It is a website that I made in React JS with Laravel like API REST to add porfolios." lg={6}><img src={villartechnologies} width={300} /><p><a className="text-white" href="http://villartechnologies.com.ve/" target="_blank">Villartechnologies</a></p></Col>
-                    <Col title="It is a website that I made in Laravel MVC and Install a Theme Admin. It is a website to create culturals events in Cordoba, Argentina" lg={6}><img src={cultura} width={300} /><p><a className="text-white" href="https://cultura-global.hopto.org/" target="_blank">Cultura Global</a></p></Col>
+                    <Col title="It is a CMS Wordpress and my Job was install, edit the CSS theme. It is a Website from Spain" lg={6}><img src={diexsur} width={300} /><p><a className="" href="http://diexsur.com/" target="_blank">Diexsur</a></p></Col>
+                    <Col title="That Work was made with Codeigniter in MVC Arquitecture and modified the CSS interface. It is a Website From Paraguay" lg={6}><img src={joven} width={300} /><p><a className="" href="https://joven.lat/" target="_blank">Joven</a></p></Col>
+                    <Col title="That Work was made in Codeigniter, install an admin template and create a CRM to clean service in Spain" lg={6}><img src={elrondaguel} width={300} /><p><a className="" href="http://control.elrondaguel.es" target="_blank">ElRondaguel</a></p></Col>
+                    <Col title="It is a website that I made in React JS with Laravel like API REST to add porfolios." lg={6}><img src={villartechnologies} width={300} /><p><a className="" href="http://villartechnologies.com.ve/" target="_blank">Villartechnologies</a></p></Col>
+                    <Col title="It is a website that I made in Laravel MVC and Install a Theme Admin. It is a website to create culturals events in Cordoba, Argentina" lg={6}><img src={cultura} width={300} /><p><a className="" href="https://cultura-global.hopto.org/" target="_blank">Cultura Global</a></p></Col>
                 </Row>
             </Col>
         );
