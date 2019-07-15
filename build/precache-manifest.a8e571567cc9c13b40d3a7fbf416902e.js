2dc96f39a85d2c59bae502455f9a7ac053e3e776
Jose Manuel Rojas Villar <villar88@gmail.com>
2019-07-15 06:32:15 -0400

    loading data last curriculum

---
+++ build/precache-manifest.a8e571567cc9c13b40d3a7fbf416902e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e2c33a55da3f2291bd7274ed2279196",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff7ef7aab025af82e481",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "12254c1af0ed0a396bda",
+    "url": "/static/css/main.6cac82a0.chunk.css"
+  },
+  {
+    "revision": "ff7ef7aab025af82e481",
+    "url": "/static/js/2.ec927668.chunk.js"
+  },
+  {
+    "revision": "12254c1af0ed0a396bda",
+    "url": "/static/js/main.5c51871a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "585b25a82544438f28c7a4a352e80857",
+    "url": "/static/media/47923552.585b25a8.jpg"
+  },
+  {
+    "revision": "0b4c1421c5df0cf95ad19a074092370f",
+    "url": "/static/media/bacground.0b4c1421.jpg"
+  },
+  {
+    "revision": "386de50daa7ed4a43064164b6113a365",
+    "url": "/static/media/cultura.386de50d.gif"
+  },
+  {
+    "revision": "ec06190bf7f5c8992a499ce6e57f0c50",
+    "url": "/static/media/diexsur.ec06190b.gif"
+  },
+  {
+    "revision": "0a704e5a7eacabdadfd35de435be3d79",
+    "url": "/static/media/elrondaguel.0a704e5a.gif"
+  },
+  {
+    "revision": "2f267ac85c8d6ff24d5ced5d36621bfd",
+    "url": "/static/media/joven.2f267ac8.gif"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "bbb19fe50d4dd48ed80f2b3905714fc6",
+    "url": "/static/media/villartechnologies.bbb19fe5.gif"
+  }
+]);
